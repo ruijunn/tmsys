4a0260747cb0154bff853bd2dd4756d77bb9c3d8
ruijunn <ruijun293@gmail.com>
2022-03-23 16:14:49 +0800

    assign user to a group

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -7,7 +7,7 @@ exports.user_group = async function(req, res) {
         res.render('createGroup', {isLoggedIn: req.session.isLoggedIn}); // redirect to create user page
     }
     // check if username belong to user group
-    if (await group.checkGroup(req.session.username, "user")) {
+    else {
         console.log("User is not an admin, not authorized!");
         res.redirect('/home'); // redirect to home page
     }
@@ -35,3 +35,78 @@ exports.user_group_create = async function(req, res) {
         res.render('createGroup', {error: 'Please enter a group name!'});
     }
 }
+
+exports.user_list = async function(req, res) {
+    if (await group.checkGroup(req.session.username, "admin")) {
+        console.log("User is an admin");
+        var userList = [];
+        db.query('SELECT username FROM accounts', function(err, rows, fields) {
+            if (err) {
+                console.log(err);
+            } else {
+                // Loop check on each row
+                for (var i = 0; i < rows.length; i++) {
+                // Create an object to save current row's data
+                    var user = {
+                        'username': rows[i].username
+                    }
+                    userList.push(user); // Add object into array
+                }
+                res.render('listUsers', {
+                    isLoggedIn: req.session.isLoggedIn, 
+                    "userList": userList
+                }); // Render assignGroup.pug page using array 
+            }
+        });
+    }
+    else {
+        console.log("User is not an admin, not authorized!");
+        res.redirect('/home'); // redirect to home page
+    }
+}
+
+var user;
+
+exports.get_user_group = async function(req, res) {
+    var username = req.params.username;
+    // console.log(username);
+    db.query('SELECT * FROM usergrp_list WHERE username = ?', [username], function(err, rows, fields) {
+        if (err) {
+            console.log(err);
+        } 
+        else {
+            user = rows;
+            // console.log(user);
+            var selectArray = [];
+            const allgrps = req.body;
+            db.query('SELECT groupName FROM usergrp', [allgrps], function(err, rows, fields) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    // Loop check on each row
+                    for (var i = 0; i < rows.length; i++) {
+                    // Create an object to save current row's data
+                        var s = {
+                            'groupname': rows[i].groupName
+                        }
+                        selectArray.push(s); // Add object into array
+                    }
+                }
+                // console.log("sss", selectArray);
+                res.render('assignGroup', {isLoggedIn: req.session.isLoggedIn, "userA": username, "userG": user, "selectArray": selectArray});
+            });
+        }
+    });
+}
+
+exports.post_user_group = async function(req, res) {
+    var username = req.params.username;
+    const {allgrps} = req.body;
+    const sql = "INSERT INTO usergrp_list (username, groupname) VALUES (?, ?)";
+    db.query(sql, [username, allgrps], function (error, result) {
+        if (error) throw error; 
+        console.log("Successfully assigned group!")
+        res.redirect('/listUsers'); 
+    });
+} 
--- routes/group.js
+++ routes/group.js
@@ -4,5 +4,8 @@ const groupController = require('../controllers/groupController');
 
 app.get('/createGroup', groupController.user_group); // Display create group form
 app.post('/createGroup', groupController.user_group_create); // Handle create group form
+app.get('/listUsers', groupController.user_list); // Display user list 
+app.get('/assignGroup/:username', groupController.get_user_group); // Display assign group form
+app.post('/assignGroup/:username', groupController.post_user_group); // Handle assign group form
 
 module.exports = app;
