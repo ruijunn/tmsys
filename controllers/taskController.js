6e023f935ea9f4a2808370f8aea401980aa24e97
ruijunn <ruijun293@gmail.com>
2022-03-31 09:13:35 +0800

    modifications for previous files

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -4,7 +4,23 @@ const group = require('../checkGroup');
 /** Display create task page */
 exports.get_create_task = async function(req, res) {
     if (await group.checkGroup(req.session.username, "project lead")) {
-        res.render('createTask', {isLoggedIn: req.session.isLoggedIn});
+        var applicationArray = [];
+        db.query('SELECT app_acronym FROM application', function(err, rows, fields) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                // Loop check on each row
+                for (var i = 0; i < rows.length; i++) {
+                // Create an object to save current row's data
+                    var app = {
+                        'appname': rows[i].app_acronym
+                    }
+                    applicationArray.push(app); // Add object into array
+                }
+            }
+            res.render('createTask', {isLoggedIn: req.session.isLoggedIn, "applicationArray": applicationArray});
+        });
     }
     else {
         console.log("Not authorized!");
