3815ebb29b2b4127ab5b02aa9521177a880f2e3a
ruijunn <ruijun293@gmail.com>
2022-04-14 16:41:09 +0800

    stored email credentials in .env file

--- email.js
+++ email.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
+
 const nodemailer = require('nodemailer');
 
+const EMAIL_HOST = process.env.EMAIL_HOST;
+const EMAIL_PORT = process.env.EMAIL_PORT;
+const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+
 var transporter = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
+    host: EMAIL_HOST,
+    port: EMAIL_PORT,
     auth: {
-      user: "a815546a4d4cfc",
-      pass: "a4b43b9f35679c"
+      user: EMAIL_USERNAME,
+      pass: EMAIL_PASSWORD
     }
 });
 
