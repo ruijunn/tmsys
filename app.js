0cd2d1c2807ecd41bc5d2b060a3e32533da04683
ruijunn <ruijun293@gmail.com>
2022-04-30 12:43:55 +0800

    installed cookie-parser module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./dbServer'); 
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -18,8 +19,10 @@ var leftFrameRouter = require('./routes/leftFrame');
 
 // Inititalize the app and add middleware
 app.set('view engine', 'pug'); // Setup the pug
+app.use(express.json()); // parses incoming JSON requests and puts the parsed data in req.body
 app.use(express.static(__dirname + '/public')); // Static files to serve CSS file
 app.use(bodyParser.urlencoded({extended: true})); // Setup the body parser to handle form submits
+app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
 app.use(session({secret: 'super-secret'})); // Session setup
 
 // Add the routes to middleware chain
